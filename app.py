51a6e2cf507a09a85b43c27211106c960a8c6870
StefanBursac <stefanbursac@hotmail.com>
2021-08-17 16:15:45 +0200

    able to view book

--- app.py
+++ app.py
@@ -53,6 +53,27 @@ def clean_price(price_str):
     else:
         return int(price_float * 100)
 
+
+def clean_id(id_str, options):
+    try:
+        book_id = int(id_str)
+
+    except ValueError:
+       input("""
+                \r******ID ERROR*******
+                \rThe id should be a number
+                \rPress any key to try again""")
+    
+    else:
+        if book_id in options:
+            return book_id
+        else:
+            input(f"""
+                \r******ID ERROR*******
+                \rOptions: {options}
+                \rPress any key to try again""")          
+            return
+
 def add_csv():
     with open("suggested_books.csv") as csvfile:
         data = csv.reader(csvfile)
@@ -95,8 +116,25 @@ def app():
                 print(f"{book.id} | {book.title} | {book.author}")
             input("Press any key to continue")
         elif choice == "3":
-            pass
-        elif choice == "4":
+            id_options = []
+            for book in session.query(Book):
+                id_options.append(book.id)
+            id_error = True
+            while id_error:
+                id_choice = input(f"""
+                    \nId Options: {id_options}
+                    \rBook id:""")
+                id_choice = clean_id(id_choice, id_options)
+                if type(id_choice) == int:
+                    id_error = False
+            the_book = session.query(Book).filter(Book.id == id_choice).first()
+            print(f"""
+                \n{the_book.title} by {the_book.author}
+                \rPublished: {the_book.published_date}
+                \rPrice: ${the_book.price / 100}
+                """)
+            input("Press any key to return to the menu")
+        elif choice == "4": 
             pass
         else:
             print("Good by")
